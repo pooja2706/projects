4c5ff56cfc2b80a955bb0f83e165e0b98fd0a59e
Pooja Choudhary <poojacdr27@gmail.com>
2022-12-22 16:16:19 +0530

    Creating project

---
+++ EncryptFiles/EncryptDecrypt.java
@@ -0,0 +1,10 @@
+// package EncryptFiles;
+
+import java.util.Scanner;
+
+public class EncryptDecrypt{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
+
